cc853f9426598d4cd9c9ffab78b60067b98c36d2
VahanAper <asryanvahandev@gmail.com>
2017-03-17 00:47:53 +0400

    Section 4 completed

---
+++ src/reducers/ReducerBooks.js
@@ -0,0 +1,8 @@
+export default function () {
+  return [
+    { title: 'Javascript: The Good Parts', pages: 101 }, //eslint-disable-line
+    { title: 'Harry Poter', pages: 102 },
+    { title: 'The Dark Tower', pages: 103 },
+    { title: 'Eloquernt Javascript', pages: 104 }
+  ];
+}
